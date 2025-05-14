8ad60b0045c990e9d3cf53a246e983f6d321bee2
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-14 11:47:22 -0600

    Better font

--- src/io/display.hpp
+++ src/io/display.hpp
@@ -4,7 +4,7 @@
 #include <string>
 namespace PiPIO {
 
-inline const int CHARPX_WIDTH = 4;
+inline const int CHARPX_WIDTH = 5;
 inline const int CHARPX_HEIGHT = 8;
 inline const int CHAR_PAD = 1;
 inline const int DISPLAY_WIDTH = 128;
--- tests/test.cpp
+++ tests/test.cpp
@@ -11,7 +11,7 @@ int main() {
   //   }
   // }
   //
-  PiPIO::drawText(10, 10, "AAAAAAAAA");
+  PiPIO::drawText(10, 10, "HelloWorld!");
 
   PiPIO::refreshDisplay();
 

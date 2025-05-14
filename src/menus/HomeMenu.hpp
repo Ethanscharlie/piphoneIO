9200b65a37e288b12737e177c2b20d75f35996c9
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-14 14:11:57 -0600

    Better home menu

--- src/menus/HomeMenu.hpp
+++ src/menus/HomeMenu.hpp
@@ -17,17 +17,29 @@ public:
     }
   }
 
-  void render() override { PiPIO::drawText(5, 16 + 5, options[selection]); }
+  void render() override {
+    std::string headerText = "";
+    for (int i = 0; i < options.size(); i++) {
+      if (i == selection) {
+        headerText += " o";
+      } else {
+        headerText += " .";
+      }
+    }
+    PiPIO::drawText(0, 0, headerText);
+
+    PiPIO::drawText(5, 16 + 5, options[selection]);
+  }
 
   void onInput(bool a, bool b, bool c, bool d) override {
-    if (a && !b && !c && !d) {
+    if (!a && !b && !c && d) {
       selection++;
       if (selection == options.size()) {
         selection = 0;
       }
     }
 
-    if (!a && !b && !c && d) {
+    if (a && !b && !c && !d) {
       selection--;
       if (selection < 0) {
         selection = options.size() - 1;

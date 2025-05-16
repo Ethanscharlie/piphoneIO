1f2c8da3c2115d5f00ca17995c72ce017b6a6d93
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-16 12:15:52 -0600

    Added stub for stopwatch menu

---
+++ src/menus/StopwatchMenu.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include "menus/Menu.hpp"
+
+class StopwatchMenu : public Menu {
+public:
+  void render() override {}
+
+  void onInput(bool a, bool b, bool c, bool d) override {}
+};

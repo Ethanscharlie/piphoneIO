20440353c564301be75ca03e7308281524d29785
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-17 10:08:37 -0600

    Getting videos from channel

--- src/menus/YouTubeMenu.hpp
+++ src/menus/YouTubeMenu.hpp
@@ -11,9 +11,10 @@
 
 class YouTubeMenu : public Menu {
   struct YTVideo {
-    std::string name;
+    std::string title;
     std::string url;
     std::string channel;
+    std::string published;
   };
 
   const std::string rssTemplate =
@@ -51,12 +52,41 @@ class YouTubeMenu : public Menu {
     return readBuffer; // Return the RSS XML as a string
   }
 
+  static std::vector<YTVideo> getChannelFeed(const std::string &rssXML) {
+    std::vector<YTVideo> videos;
+
+    tinyxml2::XMLDocument doc;
+    doc.Parse(rssXML.c_str());
+
+    tinyxml2::XMLElement *feed = doc.FirstChildElement("feed");
+
+    const std::string channel = feed->FirstChildElement("title")->GetText();
+
+    for (tinyxml2::XMLElement *entry = feed->FirstChildElement("entry");
+         entry != nullptr; entry = feed->NextSiblingElement("entry")) {
+      const std::string title = entry->FirstChildElement("title")->GetText();
+      const std::string url =
+          entry->FirstChildElement("link")->Attribute("href");
+      const std::string published =
+          entry->FirstChildElement("published")->GetText();
+
+      videos.push_back({title, url, channel, published});
+    }
+
+    return videos;
+  }
+
   void getFeed() {
     std::vector<YTVideo> videos;
 
     for (std::string channelID : channelIDs) {
-      std::string rssPage = getRssXml(rssTemplate + channelID);
-      std::cout << rssPage << "\n";
+      auto videos = getChannelFeed(getRssXml(rssTemplate + channelID));
+
+      std::cout << videos[0].title << "\n";
+      std::cout << videos[0].url << "\n";
+      std::cout << videos[0].channel << "\n";
+      std::cout << videos[0].published << "\n";
+      std::cout << "\n";
     }
   }
 

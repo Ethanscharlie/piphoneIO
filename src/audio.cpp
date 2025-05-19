16a09a5e22a3e5182f4ddd2806c18ef10086b0b2
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-18 21:03:44 -0600

    Added audio functions without functionality

---
+++ src/audio.cpp
@@ -0,0 +1,66 @@
+#include "audio.hpp"
+#include <filesystem>
+#include <iostream>
+#include <queue>
+#include <string>
+
+namespace Audio {
+
+static std::queue<std::string> audioQueue;
+static bool playing;
+
+//////////////////////////////////////////////////////////////////
+// Header functions
+//////////////////////////////////////////////////////////////////
+
+void pause() { playing = false; }
+
+void play() {
+  if (audioQueue.size() <= 0) {
+    std::cerr << "Cannot play, no files in queue\n";
+    return;
+  }
+
+  playing = true;
+}
+
+void toggle() {
+  if (playing) {
+    pause();
+  } else {
+    play();
+  }
+}
+
+void add(const std::string &file) {
+  if (!std::filesystem::exists(file)) {
+    std::cerr << "Can't add file, file not found " << file << "\n";
+    return;
+  }
+
+  audioQueue.push(file);
+}
+
+void next() {
+  if (audioQueue.size() <= 1) {
+    std::cerr << "Can't skip, only 1 or less files in queue\n";
+    return;
+  }
+
+  audioQueue.pop();
+}
+
+void clear() {
+  playing = false;
+
+  while (!audioQueue.empty()) {
+    audioQueue.pop();
+  }
+}
+
+void clearAndAdd(const std::string &file) {
+  clear();
+  add(file);
+}
+
+} // namespace Audio
---
+++ src/audio.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+namespace Audio {
+
+void pause();
+
+void play();
+
+void toggle();
+
+void add(const std::string &file);
+
+void next();
+
+void clear();
+
+void clearAndAdd(const std::string &file);
+
+} // namespace Audio
